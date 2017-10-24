74f965eddf161bbcdee6252d300b10d5aa145c87
WungaBunga <william.tornqvist@sunbyberg.it-gymnasiet.se>
2017-10-24 11:21:12 +0200

    StenSaxPåse
    Idag har vi gjort en stex sax påse generator!

---
+++ src/StenSaxPåse/StenSaxPåse.java
@@ -0,0 +1,33 @@
+package StenSaxPåse;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class StenSaxPåse {
+
+	public static void main(String[] args) {
+		
+		Scanner usersChoice = new Scanner(System.in);
+		System.out.println("Select Stone Sciccor or Paper");
+		String usersPick = usersChoice.nextLine().toLowerCase();
+		//System.out.println(usersPick);
+		
+		Random randomGenerator = new Random();
+		int stoneSiccorBag = (int) randomGenerator.nextInt(3);
+		
+		
+		
+		if (stoneSiccorBag == 0) {
+			System.out.println("Computer - Stone");
+		} else if (stoneSiccorBag == 1){
+			System.out.println("Computer - Siccor");
+		} else {
+			System.out.println("Computer - Paper");
+		}
+		
+		
+		
+	}
+	
+
+}
