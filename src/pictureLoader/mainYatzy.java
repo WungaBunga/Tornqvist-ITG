0a021feabb96e88ebbc1f6fbff1b647fd2bff3a9
WungaBunga <william.tornqvist@sunbyberg.it-gymnasiet.se>
2017-11-21 13:42:38 +0100

    PlatiunumCars
    oajgaj

--- src/pictureLoader/mainYatzy.java
+++ src/pictureLoader/mainYatzy.java
@@ -1,29 +1,25 @@
 package pictureLoader;
-
 import java.awt.Color;
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
-
 public class mainYatzy {
-
 	public static void main(String[] args) {
 		printBoard();
 
-	}
-
-	
+			}
 	
 private static void printBoard() {
-	JOptionPane.showMessageDialog(null, "Do you want to see something pretty?", "!!!WARNING!!!", JOptionPane.ERROR_MESSAGE);
-	String name = JOptionPane.showInputDialog("Yes or No");
-	JOptionPane.showMessageDialog(null, "So your answer is" + name);
-    ImageIcon icon = new ImageIcon("C:/Users/william.tornqvist1/Pictures/ginger.jpg");
-	JOptionPane.showMessageDialog(null, "Here you go!");
-	
-	
+	JOptionPane.showMessageDialog(null, "Välkommen till PlatinumCars AB", "PlatinumCars Norrköping", JOptionPane.ERROR_MESSAGE);
+	String name = JOptionPane.showInputDialog("Vänligen skriv in den bilen du söker efter");
+	JOptionPane.showMessageDialog(null, "Söker efter" +  name);
+	JOptionPane.showMessageDialog(null, "Här har du resultaten: 19 stycken bilar hittade vi i våran databas");
 	/* Det kommer upp en pop-up ruta på din skärm där datorn frågar vad du heter, och du ska skriva in ditt namn och sen kommer det upp det namnet du skrev.
 	 */
-	
+	for(int i=1; i<1; i++){
+        System.out.println(": " + i);
+			}
+		}
 	}
-}
+
